ac337a015144b794b463a77efd23eeb9ceac98b8
Dixon Pedraza <pedrazadixon@gmail.com>
2025-04-15 17:57:18 -0500

    migrate tabs to alpine

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,50 +1,113 @@
 <?= $this->extend('default') ?>
 
+
+<?= $this->section('before_close_head') ?>
+<script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+<?= $this->endSection() ?>
+
+
 <?= $this->section('content') ?>
 
-<div class="mb-4 border-b border-gray-200 dark:border-gray-700">
-    <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="main-tabs" data-tabs-toggle="#main-tabs-content" role="tablist">
-        <li class="me-2" role="presentation">
-            <button class="inline-block cursor-pointer p-4 border-b-2 rounded-t-lg" data-tabs-target="#url-tab" type="button" role="tab" aria-controls="url-tab" aria-selected="false">
-                Short a URL
-            </button>
-        </li>
-        <li class="me-2" role="presentation">
-            <button class="inline-block cursor-pointer p-4 border-b-2 rounded-t-lg" data-tabs-target="#qr-tab" type="button" role="tab" aria-controls="qr-tab" aria-selected="false">
-                QR Code
-            </button>
-        </li>
-        <li class="me-2" role="presentation">
-            <button class="inline-block cursor-pointer p-4 border-b-2 rounded-t-lg" data-tabs-target="#note-tab" type="button" role="tab" aria-controls="note-tab" aria-selected="false">
-                Share a note
-            </button>
-        </li>
-        <li class="me-2" role="presentation">
-            <button class="inline-block cursor-pointer p-4 border-b-2 rounded-t-lg" data-tabs-target="#link-group-tab" type="button" role="tab" aria-controls="link-group-tab" aria-selected="false">
-                Link group
-            </button>
-        </li>
-    </ul>
-</div>
+<div x-data="tabsApp">
 
-<div id="main-tabs-content">
+    <div class="mb-4 border-b border-gray-200 dark:border-gray-700">
+        <ul class="flex flex-wrap -mb-px text-sm font-medium text-center text-gray-500 dark:text-gray-400" role="tablist">
+            <li class="me-2" role="presentation">
+                <button
+                    class="inline-block p-4"
+                    :class="{
+                        'border-b-2 text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500': activeTab === 'url-tab',
+                        'border-b-2 border-transparent hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300': activeTab !== 'url-tab',
+                    }"
+                    type="button"
+                    x-on:click="activeTab = 'url-tab'"
+                    role="tab">
+                    Short a URL
+                </button>
+            </li>
+            <li class="me-2" role="presentation">
+                <button
+                    class="inline-block p-4"
+                    :class="{
+                        'border-b-2 text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500': activeTab === 'qr-tab',
+                        'border-b-2 border-transparent hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300': activeTab !== 'qr-tab',
+                    }"
+                    type="button"
+                    x-on:click="activeTab = 'qr-tab'"
+                    role="tab">
+                    QR Code
+                </button>
+            </li>
+            <li class="me-2" role="presentation">
+                <button
+                    class="inline-block p-4"
+                    :class="{
+                        'border-b-2 text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500': activeTab === 'note-tab',
+                        'border-b-2 border-transparent hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300': activeTab !== 'note-tab',
+                    }"
+                    type="button"
+                    x-on:click="activeTab = 'note-tab'"
+                    role="tab">
+                    Share a note
+                </button>
+            </li>
+            <li role="presentation">
+                <button
+                    class="inline-block p-4"
+                    :class="{
+                        'border-b-2 text-blue-600 hover:text-blue-600 dark:text-blue-500 dark:hover:text-blue-500 border-blue-600 dark:border-blue-500': activeTab === 'linkgroup-tab',
+                        'border-b-2 border-transparent hover:border-gray-300 hover:text-gray-600 dark:hover:text-gray-300': activeTab !== 'linkgroup-tab',
+                    }"
+                    type="button"
+                    x-on:click="activeTab = 'linkgroup-tab'"
+                    role="tab">
+                    Link group
+                </button>
+            </li>
+        </ul>
+    </div>
 
-    <div class="p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="url-tab" role="tabpanel" aria-labelledby="url-tab">
-        <?= form_open(base_url('generate')); ?>
-        <label for="email-address-icon" class="block mb-3 text-sm font-medium text-gray-900 dark:text-white">
-            Shorten a long URL
-        </label>
-        <div class="relative mb-3">
-            <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.213 9.787a3.391 3.391 0 0 0-4.795 0l-3.425 3.426a3.39 3.39 0 0 0 4.795 4.794l.321-.304m-.321-4.49a3.39 3.39 0 0 0 4.795 0l3.424-3.426a3.39 3.39 0 0 0-4.794-4.795l-1.028.961" />
-                </svg>
+
+    <?= form_open(base_url('generate')); ?>
+
+    <div class="p-4 rounded-lg bg-gray-50 dark:bg-gray-800">
+
+        <template x-if="activeTab == 'url-tab' || activeTab == 'qr-tab'">
+            <div>
+                <label for="email-address-icon" class="block mb-3 text-sm font-medium text-gray-900 dark:text-white">
+                    Shorten a long URL
+                </label>
+                <div class="relative mb-3">
+                    <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                        <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.213 9.787a3.391 3.391 0 0 0-4.795 0l-3.425 3.426a3.39 3.39 0 0 0 4.795 4.794l.321-.304m-.321-4.49a3.39 3.39 0 0 0 4.795 0l3.424-3.426a3.39 3.39 0 0 0-4.794-4.795l-1.028.961" />
+                        </svg>
+                    </div>
+                    <input type="url" autocomplete="off" name="url" :required="activeTab == 'url-tab' || activeTab == 'qr-tab'" value="<?= set_value('url') ?>" placeholder="https://example.com/my-long-url..." class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                </div>
             </div>
-            <input type="url" autocomplete="off" name="url" value="<?= set_value('url') ?>" required placeholder="https://example.com/my-long-url..." class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+        </template>
+
+        <template x-if="activeTab == 'note-tab'">
+            <div>
+                <label for="email-address-icon" class="block mb-3 text-sm font-medium text-gray-900 dark:text-white">
+                    Share a note
+                </label>
+                <div class="relative mb-3">
+                    <textarea name="note" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+                </div>
+            </div>
+        </template>
+
+
+        <div x-cloak x-show="activeTab == 'linkgroup-tab'">
+            <p class="mb-3 text-sm font-medium text-gray-900 dark:text-white">
+                You need be logged in to create a link group.
+            </p>
         </div>
 
 
-        <div data-accordion="collapse">
+        <div data-accordion="collapse" x-show="activeTab == 'url-tab' || activeTab == 'qr-tab' || activeTab == 'note-tab'">
             <h2 id="accordion-collapse-heading-1">
                 <?php $expanded = ! empty(array_diff(array_keys(validation_errors()), ['url'])) ? 'true' : 'false'; ?>
                 <button type="button" class="flex items-center w-full text-sm font-medium rtl:text-right text-gray-500 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="<?= $expanded ?>" aria-controls="accordion-collapse-body-1">
@@ -117,96 +180,32 @@
             </div>
         </div>
 
-        <?php if (count(validation_errors()) > 0): ?>
-            <div class="flex items-center p-4 mt-3 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
-                <div>
-                    <?= validation_list_errors() ?>
+        <div x-show="activeTab == 'url-tab' || activeTab == 'qr-tab' || activeTab == 'note-tab'">
+            <?php if (count(validation_errors()) > 0): ?>
+                <div class="flex items-center p-4 mt-3 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400 dark:border-red-800" role="alert">
+                    <div>
+                        <?= validation_list_errors() ?>
+                    </div>
                 </div>
-            </div>
-        <?php endif; ?>
-
-
-        <div class="mt-3">
-            <?= form_submit('submit_url', 'Shorten', [
-                'class' => 'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
-            ]); ?>
+            <?php endif; ?>
         </div>
-        <?= form_close(); ?>
-    </div>
 
-    <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="qr-tab" role="tabpanel" aria-labelledby="qr-tab">
-
-        <div class="dark:text-gray-200" role="alert">Commig soon...</div>
-
-        <?php /*
-        <form>
-            <label for="email-address-icon" class="block mb-3 text-sm font-medium text-gray-900 dark:text-white">
-                Create a QR Code
-            </label>
-            <div class="relative mb-3">
-                <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-                    <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.213 9.787a3.391 3.391 0 0 0-4.795 0l-3.425 3.426a3.39 3.39 0 0 0 4.795 4.794l.321-.304m-.321-4.49a3.39 3.39 0 0 0 4.795 0l3.424-3.426a3.39 3.39 0 0 0-4.794-4.795l-1.028.961" />
-                    </svg>
-                </div>
-                <input type="text" id="email-address-icon" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="https://example.com/my-long-url..." />
-            </div>
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Create
+        <div class="mt-3" x-show="activeTab == 'url-tab' || activeTab == 'qr-tab' || activeTab == 'note-tab'">
+            <button name="submit" :value="activeTab" type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <span x-show="activeTab == 'url-tab'">Shorten</span>
+                <span x-cloak x-show="activeTab == 'qr-tab'">Generate QR</span>
+                <span x-cloak x-show="activeTab == 'note-tab'">Share</span>
+                <span x-cloak x-show="activeTab == 'linkgroup-tab'">Create</span>
             </button>
-        </form>
-        */ ?>
+        </div>
 
     </div>
 
-    <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="note-tab" role="tabpanel" aria-labelledby="note-tab">
-
-        <div class="dark:text-gray-200" role="alert">Commig soon...</div>
-
-        <?php /*
-        <form>
-            <label for="email-address-icon" class="block mb-3 text-sm font-medium text-gray-900 dark:text-white">
-                Share a note
-            </label>
-            <div class="relative mb-3">
-
-                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
-
-            </div>
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Share
-            </button>
-        </form>
-        */ ?>
-
-    </div>
-
-    <div class="hidden p-4 rounded-lg bg-gray-50 dark:bg-gray-800" id="link-group-tab" role="tabpanel" aria-labelledby="link-group-tab">
-
-        <div class="dark:text-gray-200" role="alert">Commig soon...</div>
-
-        <?php /*
-        <form>
-            <label for="email-address-icon" class="block mb-3 text-sm font-medium text-gray-900 dark:text-white">
-                Share a note
-            </label>
-            <div class="relative mb-3">
-
-                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
-
-            </div>
-            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Share
-            </button>
-        </form>
-        */ ?>
-
-    </div>
+    <?= form_close(); ?>
 
 </div>
 
 <?= $this->endSection() ?>
-
 
 
 <?= $this->section('before_close_body') ?>
@@ -230,5 +229,19 @@
 
     expirationHandler();
 </script>
+
+
+<script>
+    (function() {
+
+        document.addEventListener('alpine:init', () => {
+            Alpine.data('tabsApp', () => ({
+                activeTab: 'url-tab',
+            }));
+        });
+
+    })();
+</script>
+
 
 <?= $this->endSection() ?>
