df6f724a19732b75b8ce7fbf6230cc19e8693d63
Dixon Pedraza <pedrazadixon@gmail.com>
2025-04-19 21:39:35 -0500

    feat: add POST route for redirectOrShow to handle form submissions

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,4 @@ $routes->get('/s/(:any)', 'Home::share/$1');
 $routes->get('/p/(:any)', 'Home::protected/$1');
 $routes->post('/p/(:any)', 'Home::protected/$1');
 $routes->get('(:any)', 'Home::redirectOrShow/$1');
+$routes->post('(:any)', 'Home::redirectOrShow/$1');
